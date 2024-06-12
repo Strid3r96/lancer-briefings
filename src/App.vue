f6c43583d71b8b4287f3a97e690cb9de977ea596
Strid3r96 <waywardstrid3r@gmail.com>
2024-06-12 16:33:01 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,17 +95,17 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
-          "status": "start"
+          "name": "The Drop",
+          "status": "success"
         },
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
+          "callsign": "Saurus",
+          "alias": "Raenna",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Everest",
+          "corpro": "Horus",
+          "frame": "Balor",
           "mech": "Type - Worst Beat"
         },
         {

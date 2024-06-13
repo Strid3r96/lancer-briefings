abb77a87acf76ce110a59f7a9baaea1ec9cb2b4f
Strid3r96 <waywardstrid3r@gmail.com>
2024-06-12 18:04:49 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -127,9 +127,9 @@ export default {
           "callsign": "Acolyte",
           "alias": "Vander Douglin",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Necrophile"
+          "corpro": "Harrison Armory",
+          "frame": "Iskander",
+          "mech": "Bethelhem"
         },
         {
           "callsign": "Outlaw",

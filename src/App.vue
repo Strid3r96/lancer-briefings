d9cb3e40d58c179d3de09aeed3d5529014f3c562
Strid3r96 <waywardstrid3r@gmail.com>
2024-06-26 12:27:01 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -113,8 +113,8 @@ export default {
           "alias": "Kadira",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Chomolungma"
+          "frame": "Chomolungma",
+          "mech": "Something Is Missing"
         },
         {
           "callsign": "Scor",
